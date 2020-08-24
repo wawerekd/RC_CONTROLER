c33705f014cbee58dc2b6a6e7e2187bf7d02ac07
Damian Wawerek <testbitrix@underant.com>
2020-08-24 10:41:38 +0200

    Reorgatniaziton of folder.

--- LongPressTest/Src/timedTasks.c
+++
@@ -1,9 +0,0 @@
-/*
- * timedTasks.c
- *
- *  Created on: 24.01.2020
- *      Author: dwawerek
- */
-
-
-
---
+++ TEST_CODES/LongPressTest/Src/timedTasks.c
@@ -0,0 +1,9 @@
+/*
+ * timedTasks.c
+ *
+ *  Created on: 24.01.2020
+ *      Author: dwawerek
+ */
+
+
+
